773e0fe24fc2d24af999cf644e576839314ecad6
RaphielHS <70839561+RaphielHS@users.noreply.github.com>
2021-08-30 09:35:32 +0700

    Add files via upload

--- ChanChanAuth/Client.cs
+++ ChanChanAuth/Client.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using Leaf.xNet;
 using Fernet;
-using ChanChanAuth;
-using System.Web.Script.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace ChanChanAuth
 {
@@ -97,7 +93,24 @@ namespace ChanChanAuth
             {
             }
         }
-        
+        [Serializable]
+        class InvaldLength : Exception
+        {
+            public InvaldLength()
+            {
+            }
+
+            public InvaldLength(string message)
+                : base(message)
+            {
+            }
+
+            public InvaldLength(string message, Exception inner)
+                : base(message, inner)
+            {
+            }
+        }
+
         private static string ClientKey;
         private static byte[] ClientSecret;
         private static string ClientAid;
@@ -136,71 +149,64 @@ namespace ChanChanAuth
         /// </example>
         public static string Authencate(string username = "", string password = "", string hwid = "")
         {
-            if (ClientAid != "" || ClientKey != "")
-            {
-                using (HttpRequest cl = new HttpRequest()
-                {
-                    KeepAlive = true,
-                    IgnoreProtocolErrors = true
-                })
-                {
-                    string SessionID = sessionID(40);
-                    var PayLoad = "{\"username\": \"" + username + "\",\"password\": \"" + password + "\",\"hwid\": \"" + hwid + "\",\"sessionID\": \"" + SessionID + "\"}";
-                    try
-                    {
-                        string encrypedPayload = SimpleFernet.Encrypt(ClientSecret, PayLoad.Replace("\\\"", "\"").ToBase64String().UrlSafe64Decode());
-                        cl.AddHeader("data", encrypedPayload);
-                        cl.AddHeader("aid", ClientAid);
-                        var prms = new RequestParams();
-                        prms["key"] = ClientKey;
-                        string resp = cl.Get("https://api.ccauth.app/api/v3/authenticate", prms).ToString();
-                        JavaScriptSerializer js = new JavaScriptSerializer();
-                        var data = js.Deserialize<dynamic>(SimpleFernet.Decrypt(ClientSecret, resp, out var timestamp).UrlSafe64Encode().FromBase64String());
-                        if (data["error"] == "True")
-                        {
-                            throw new UnknownError("Unknown Error, Please Report to the developer.");
-                        }
-                        else
-                        {
-                            if (data["is_Authenticated"] == "True" && data["session_ID"] == SessionID)
-                            {
-                                return "Authencated";
-                            }
-                            else if (data["invalid_hwid"] == "True")
-                            {
-                                return "Invalid HWID";
-                            }
-                            else if (data["expired_license"] == "True")
-                            {
-                                return "Expired";
-                            }
-
-                            else if (data["invalid_credentials"] == "True")
-                            {
-                                return "Invalid Creds";
-                            }
-                            else
-                            {
-                                return "Unknown Response.";
-                            }
-                        }
-                    }
-                    catch (Exception err)
-                    {
-                        if (err.ToString().Contains("not a valid Base-64"))
-                            throw new InvalidSecret("Client Secret is not Base64! Please ReCheck");
-                        return "Error: " + err.ToString();
-                    }
-                }
-            }
+            if (string.IsNullOrEmpty(username) || username.Length < 3)
+                throw new InvaldLength("username Length must be more than 2!");
+            else if (string.IsNullOrEmpty(password) || password.Length < 3)
+                throw new InvaldLength("password Length must be more than 2!");
+            else if (string.IsNullOrEmpty(hwid) || hwid.Length < 3)
+                throw new InvaldLength("username Length must be more than 2!");
             else
             {
-                throw new EmptyRequiredInfo("Some Required Keys Are Required!, Please Re-Initiate");
+                if (ClientAid != "" || ClientKey != "")
+                {
+                    using (HttpRequest cl = new HttpRequest()
+                    {
+                        KeepAlive = true,
+                        IgnoreProtocolErrors = true
+                    })
+                    {
+                        string SessionID = sessionID(40);
+                        var PayLoad = "{\"username\": \"" + username + "\",\"password\": \"" + password + "\",\"hwid\": \"" + hwid + "\",\"sessionID\": \"" + SessionID + "\"}";
+                        try
+                        {
+                            string encrypedPayload = SimpleFernet.Encrypt(ClientSecret, PayLoad.Replace("\\\"", "\"").ToBase64String().UrlSafe64Decode());
+                            cl.AddHeader("data", encrypedPayload);
+                            cl.AddHeader("aid", ClientAid);
+                            var prms = new RequestParams();
+                            prms["key"] = ClientKey;
+                            string resp = cl.Get("https://api.ccauth.app/api/v3/authenticate", prms).ToString();
+                            var data = JObject.Parse(SimpleFernet.Decrypt(ClientSecret, resp, out var timestamp).UrlSafe64Encode().FromBase64String());
+                            if ((string)data["error"] == "True")
+                                throw new UnknownError("Unknown Error, Please Report to the developer.");
+                            else
+                            {
+                                if ((string)data["is_Authenticated"] == "True" && (string)data["session_ID"] == SessionID)
+                                    return "Authencated";
+                                else if ((string)data["invalid_hwid"] == "True")
+                                    return "Invalid HWID";
+                                else if ((string)data["expired_license"] == "True")
+                                    return "Expired";
+                                else if ((string)data["invalid_credentials"] == "True")
+                                    return "Invalid Creds";
+                                else
+                                    return "Unknown Response.\nResp : " + resp.ToString();
+                            }
+                        }
+                        catch (Exception err)
+                        {
+                            if (err.ToString().Contains("not a valid Base-64"))
+                                throw new InvalidSecret("Client Secret is not Base64! Please ReCheck");
+                            return "Error: " + err.ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    throw new EmptyRequiredInfo("Some Required Keys Are Required!, Please Re-Initiate");
+                }
             }
         }
-        /*
-         * Register using a Registeration key
-         */
+
         /// <summary>
         /// Authencate (or login), Authencates user with username, password and HWID
         /// </summary>
@@ -212,65 +218,136 @@ namespace ChanChanAuth
         /// </example>
         public static string Register(string discord = "", string username = "", string RegKey = "", string password = "", string hwid = "")
         {
-            if (ClientAid != "" || ClientKey != "")
-            {
-                using (HttpRequest cl = new HttpRequest()
-                {
-                    KeepAlive = true,
-                    IgnoreProtocolErrors = true
-                })
-                {
-                    string SessionID = sessionID(40);
-                    var PayLoad = "{\"username\": \"" + username + "\",\"password\": \"" + password + "\",\"hwid\": \"" + hwid + "\",\"sessionID\": \"" + SessionID + "\"}";
-                    try
-                    {
-                        cl.AddHeader("user", username);
-                        cl.AddHeader("pass", password);
-                        cl.AddHeader("regkey", RegKey);
-                        cl.AddHeader("discord", discord);
-                        cl.AddHeader("aid", ClientAid);
-                        cl.AddHeader("hwid", hwid);
-
-                        var prms = new RequestParams();
-                        prms["key"] = ClientKey;
-                        string resp = cl.Get("https://api.ccauth.app/api/v2/register", prms).ToString();
-                        JavaScriptSerializer js = new JavaScriptSerializer();
-                        var data = js.Deserialize<dynamic>(resp);
-                        if (data["error"] == "True")
-                        {
-                            throw new UnknownError("Unknown Error, Please Report to the developer.");
-                        }
-                        else
-                        {
-                            if (data["success"] == "True")
-                            {
-                                return "Registered Successfully";
-                            }
-                            else if (data["registration_enabled"] == "False")
-                            {
-                                return "Registeration isint enabled.";
-                            }
-                            else if (data["invalid_key"] == "True")
-                            {
-                                return "Invalid Key";
-                            }
-                            else
-                            {
-                                return "Unknown Response";
-                            }
-                        }
-                    }
-                    catch (Exception err)
-                    {
-                        return "Error " + err.ToString();
-                    }
-                }
-            }
+            if (string.IsNullOrEmpty(username) || username.Length < 3)
+                throw new InvaldLength("username Length must be more than 2!");
+            else if (string.IsNullOrEmpty(RegKey) || RegKey.Length < 3)
+                throw new InvaldLength("RegKey Length must be more than 2!");
+            else if (string.IsNullOrEmpty(password) || password.Length < 3)
+                throw new InvaldLength("password Length must be more than 2!");
+            else if (string.IsNullOrEmpty(hwid) || hwid.Length < 3)
+                throw new InvaldLength("username Length must be more than 2!");
             else
             {
-                throw new EmptyRequiredInfo("Some Required Keys Are Required!, Please Re-Initiate");
+                if (ClientAid != "" || ClientKey != "")
+                {
+                    using (HttpRequest cl = new HttpRequest()
+                    {
+                        KeepAlive = true,
+                        IgnoreProtocolErrors = true
+                    })
+                    {
+                        string SessionID = sessionID(40);
+                        var PayLoad = "{\"username\": \"" + username + "\",\"password\": \"" + password + "\",\"hwid\": \"" + hwid + "\",\"sessionID\": \"" + SessionID + "\"}";
+                        try
+                        {
+                            cl.AddHeader("user", username);
+                            cl.AddHeader("pass", password);
+                            cl.AddHeader("regkey", RegKey);
+                            cl.AddHeader("discord", discord);
+                            cl.AddHeader("aid", ClientAid);
+                            cl.AddHeader("hwid", hwid);
+
+                            var prms = new RequestParams();
+                            prms["key"] = ClientKey;
+                            string resp = cl.Get("https://api.ccauth.app/api/v2/register", prms).ToString();
+                            var data = JObject.Parse(resp);
+                            if ((string)data["error"] == "True")
+                                throw new UnknownError("Unknown Error, Please Report to the developer.");
+                            else
+                            {
+                                if ((string)data["success"] == "True")
+                                    return "Registered Successfully";
+                                else if ((string)data["registration_enabled"] == "False")
+                                    return "Registeration isint enabled.";
+                                else if ((string)data["invalid_key"] == "True")
+                                    return "Invalid Key";
+                                else
+                                    return "Unknown Response";
+                            }
+                        }
+                        catch (Exception err)
+                        {
+                            return "Error " + err.ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    throw new EmptyRequiredInfo("Some Required Keys Are Required!, Please Re-Initiate");
+                }
             }
         }
+        /// <summary>
+        /// Reset HWID For a user
+        /// </summary>
+        /// <param name="username">User Registered Username</param>
+        /// <param name="password">User Registered Password</param>
+        /// <param name="hwid">The New Unique Hardware Identification ID from the user Device</param>
+        /// <param name="key">The Reset Key</param>
+        /// <example>
+        /// ChanChanAuth.ResetHWID(username: "Raphiel", password: "Raphiel1124", hwid: "HWID-GOES-HERE", key: "HWID Reset Key");
+        /// </example>
+        public static string ResetHWID(string username, string password, string hwid, string key)
+        {
+            if (string.IsNullOrEmpty(username) || username.Length < 3)
+                throw new InvaldLength("username Length must be more than 2!");
+            else if (string.IsNullOrEmpty(key) || key.Length < 3)
+                throw new InvaldLength("Key Length must be more than 2!");
+            else if (string.IsNullOrEmpty(password) || password.Length < 3)
+                throw new InvaldLength("password Length must be more than 2!");
+            else if (string.IsNullOrEmpty(hwid) || hwid.Length < 3)
+                throw new InvaldLength("username Length must be more than 2!");
+            else
+            {
+                if (ClientAid != "" || ClientKey != "")
+                {
+                    using (HttpRequest cl = new HttpRequest()
+                    {
+                        KeepAlive = true,
+                        IgnoreProtocolErrors = true
+                    })
+                    {
+                        string SessionID = sessionID(40);
+                        var PayLoad = "{\"username\": \"" + username + "\",\"password\": \"" + password + "\",\"hwid\": \"" + hwid + "\",\"sessionID\": \"" + SessionID + "\"}";
+                        try
+                        {
+                            cl.AddHeader("user", username);
+                            cl.AddHeader("pass", password);
+                            cl.AddHeader("newhwid", hwid);
+                            cl.AddHeader("hwidresetkey", key);
+                            cl.AddHeader("aid", ClientAid);
 
+                            var prms = new RequestParams();
+                            prms["key"] = ClientKey;
+                            string resp = cl.Get("https://api.ccauth.app/api/v3/reset", prms).ToString();
+                            var data = JObject.Parse(resp);
+                            if ((string)data["error"] == "True")
+                                throw new UnknownError("Unknown Error, Please Report to the developer.");
+                            else
+                            {
+                                if ((string)data["success"] == "True")
+                                    return "Reset Successfully";
+                                else if ((string)data["hwid_resets"] == "False")
+                                    return "Restes isint enabled.";
+                                else if ((string)data["invalid_key"] == "True")
+                                    return "Invalid Key";
+                                else if ((string)data["reset_today"] == "True")
+                                    return "User has Reseted today";
+                                else
+                                    return "Unknown Response";
+                            }
+                        }
+                        catch (Exception err)
+                        {
+                            return "Error " + err.ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    throw new EmptyRequiredInfo("Some Required Keys Are Required!, Please Re-Initiate");
+                }
+            }
+        }
     }
 }
